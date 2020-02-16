13fa14d15f1c8b5f5726685e1a8dd7215e777c0e
bronz3beard <info2rory@gmail.com>
2020-02-16 18:01:56 +1000

    fix overflow for modal

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import DragAndDrop from "./components/dragAndDrop";
 // import Animation from './components/canvasParticles';
 // import Preloader from './components/loadSpinner';
 // import OnPageFilter from './components/pageFilter';
-// import DataTable from './components/dataTable';
+import DataTable from "./components/dataTable";
 // import DragAndDrop from './components/dragAndDrop';
 // import ContextMenu from './components/customContextMenu';
 import "./App.css";
@@ -18,21 +18,24 @@ import "./App.css";
 //   { imageAlt: "3", imageUrl: "https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" },
 //   { imageUrl: "https://images.unsplash.com/photo-1541233349642-6e425fe6190e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80", imageAlt: "4" }
 // ];
-// const columns =[
-//   {
-//       Header: "Id",
-//       Value: "id",
-//   }, {
-//       Header: "Content",
-//       Value: "body",
-//   }, {
-//       Header: "Email",
-//       Value: "email",
-//   }, {
-//       Header: "Name",
-//       Value: "name",
-//   },
-// ]
+const columns = [
+  {
+    Header: "Id",
+    Value: "id"
+  },
+  {
+    Header: "Content",
+    Value: "body"
+  },
+  {
+    Header: "Email",
+    Value: "email"
+  },
+  {
+    Header: "Name",
+    Value: "name"
+  }
+];
 
 // const customLinks = [
 //   {
@@ -69,29 +72,29 @@ import "./App.css";
 //   }
 // ];
 function App() {
-  // const [data, setData] = useState([]);
-  // console.log(data);
-  // const [hasLoader, setHasLoader] = useState(true);
-  //   const loadRecordsFromServer = () => {
-  //       const xmlhr = new XMLHttpRequest();
-  //       const url = "https://jsonplaceholder.typicode.com/comments";
-  //       xmlhr.open("GET", url, true);
-  //       xmlhr.onload = () => {
-  //           if (xmlhr.readyState === xmlhr.DONE) {
-  //               if (xmlhr.status === 200) {
-  //                   const data = JSON.parse(xmlhr.responseText);
-  //                   setData(data);
-  //                   setHasLoader(false);
-  //               }
-  //           }
-  //       };
-  //       xmlhr.send();
-  //   };
+  const [data, setData] = useState([]);
+  console.log(data);
+  const [hasLoader, setHasLoader] = useState(true);
+  const loadRecordsFromServer = () => {
+    const xmlhr = new XMLHttpRequest();
+    const url = "https://jsonplaceholder.typicode.com/comments";
+    xmlhr.open("GET", url, true);
+    xmlhr.onload = () => {
+      if (xmlhr.readyState === xmlhr.DONE) {
+        if (xmlhr.status === 200) {
+          const data = JSON.parse(xmlhr.responseText);
+          setData(data);
+          setHasLoader(false);
+        }
+      }
+    };
+    xmlhr.send();
+  };
 
-  //   useEffect(() => {
-  //     loadRecordsFromServer();
-  //   }, [])
-  //   const [query, setQuery] = React.useState('');
+  useEffect(() => {
+    loadRecordsFromServer();
+  }, []);
+  const [query, setQuery] = React.useState("");
 
   //   const searchFilter = event => {
   //     const searchValue = event.target.value;
@@ -129,9 +132,12 @@ function App() {
       {/* <Preloader /> */}
       {/* <Animation /> */}
       <Modal showModal backgroundColor="blue" handleModal={handleModal}>
-        <DragAndDrop>
-          <div type="file" name="files" accept="image/*" multiple></div>
-        </DragAndDrop>
+        <DataTable
+          data={data}
+          columns={columns}
+          hasLoader={hasLoader}
+          totalRecordsPerPage={30}
+        />
       </Modal>
       {/* <VideoPlayer
         url="https://vimeo.com/169599296"
